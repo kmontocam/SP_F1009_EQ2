4f74f94a9dce59d15572f25933a5d1f533c9f8d0
Kevin Montoya <A01740352@itesm.mx>
2021-11-22 17:41:41 -0700

    [ENHANCEMENT] Function organization
    Testing main.ipynb and release of functions

---
+++ create_database.py
@@ -0,0 +1,32 @@
+import os
+import numpy as np
+import pandas as pd
+from scipy.io import wavfile
+from fingerprinter import fingerprinter
+from compressor import compressor11025
+
+## Type your file directory full of .wav songs
+## in the variable named 'directory'
+## Make sure the directory you choose is inside this project
+
+directory = r'/Users/kmontocam/Documents/GitHub/tracks'
+
+MUSIC_FOLDER = os.listdir(directory)
+tracks = [file for file in MUSIC_FOLDER if (file[-4:] == '.wav')]
+
+fingerprints = pd.DataFrame()
+
+for track in tracks:
+
+    song_freq, song = wavfile.read(os.path.join(directory, track))
+
+    compressed_freq, song_compressed = compressor11025(song_freq, song)
+
+    freq_times, freq_highs = fingerprinter(compressed_freq, song_compressed)
+
+    data = pd.DataFrame(list(zip(freq_times, freq_highs)),
+    columns = pd.MultiIndex.from_tuples([(track[:-4], 'time'), (track[:-4], 'hz')]))
+    
+    fingerprints = pd.concat([fingerprints, data], axis = 1)
+
+fingerprints.to_csv('fingerprints.csv', index = False)
